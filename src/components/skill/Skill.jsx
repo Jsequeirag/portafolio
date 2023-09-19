0cd73a0a582aa7221cfd493177de90bfdc3bea42
Jose Luis Sequeraira  Góngora <joseluissequeirag2@gmail.com>
2023-09-19 07:40:38 -0600

    update

--- src/components/skill/Skill.jsx
+++ src/components/skill/Skill.jsx
@@ -13,12 +13,12 @@ export default function Skill() {
           >
             <h2 className="card-title">Backend</h2>
             <div className="">
-              <p>-Nodejs</p>
-              <p>-Typescript</p>
-              <p>-Expressjs</p>
-              <p>-Mongo</p>
+              <p>-NODEJS</p>
+              <p>-TYPESCRIPT</p>
+              <p>-EXPRESSJS</p>
+              <p>-NESTJS</p>
+              <p>-MONGO</p>
               <p>-SQL</p>
-              <p>-ORM(Sequelize)</p>
             </div>
           </div>
           <div
@@ -27,7 +27,7 @@ export default function Skill() {
           >
             <h2 className="card-title">Frontend</h2>
             <div className="">
-              <p>-React</p>
+              <p>-REACT</p>
               <p>-HTML,CSS,Javascript</p>
             </div>
           </div>
@@ -38,8 +38,8 @@ export default function Skill() {
           >
             <h2 className="card-title">Services</h2>
             <div className="">
-              <p>-Heroku</p>
-              <p>-Netlify</p>
+              <p>-HEROKU</p>
+              <p>-NETLIFY</p>
             </div>
           </div>
         </div>
